979a4aed46e2b03dc51241b7e3cbf5d171cbb002
kentforth <yashaar@mail.ru>
2020-09-12 15:46:27 +0700

    add swagger documentation

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const connection = mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+module.exports = connection;
--- main.js
+++ main.js
@@ -3,14 +3,13 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const mongoose = require('mongoose');
+let database = require('./config/database');
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+const swaggerUi = require('swagger-ui-express');
 
+swaggerDocument = require('./swagger.json');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(cors());
